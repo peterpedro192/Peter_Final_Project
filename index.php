be8807213c96b6db4e25f32633e9257013c7bff5
designstudio_2 <designstudio_2@192.168.1.195>
2013-08-21 16:07:21 +0100

    Added index file

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brighton Comic Arts Festival</title>
+    <link rel="stylesheet" href="core.css" type="text/css" />
+  </head>
+  
+  
+  <body>
+  <div id="wrapper">
+      <header class="mainheader">
+          
+          <img src="assets/img/logo-by-itself.gif" alt="Brighton Comic Arts Festival Logo" />
+          
+          <nav>
+              <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Events</a></li>
+                  <li><a href="#">Merchandise</a></li>
+                  <li><a href="#">Contact Us</a></li>
+              </ul>
+          </nav>
+      </header>
+      
+      <section class="maincontent">
+          <section class="content">
+              <article class="toparticle">
+                  <header class="articleheader">
+                      <h2><a href="#" title="Article post">First Post</a></h2>
+                  </header>
+                  <footer class="articlefoot">
+                      <p>This article written by Peter</p>
+                  </footer>
+                  <div class="articlecontent">
+                      <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor mi, porta non viverra eu, lacinia a dolor.                                   Curabitur vel molestie augue. Suspendisse ultricies velit odio, a hendrerit sapien interdum eu. Vestibulum at quam                               luctus, varius velit quis, suscipit purus. Nam interdum urna id purus ullamcorper porttitor. Suspendisse eros dui,                               dignissim ac libero ac, semper imperdiet tellus. Praesent dictum iaculis mi eget porta. Vivamus vulputate libero ac                              tincidunt tempor. Aenean placerat consectetur ullamcorper.                                                                                                                     
+                      </p>    
+                  </div>
+              </article>
+              <article class="bottomarticle">
+                  <header class="articleheader">
+                      <h2><a href="#" title="Article post">Second Post</a></h2>
+                  </header>
+                  <footer class="articlefoot">
+                      <p>This article written by Peter</p>
+                  </footer>
+                  <div class="articlecontent">
+                      <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor mi, porta non viverra eu, lacinia a dolor.                                   Curabitur vel molestie augue. Suspendisse ultricies velit odio, a hendrerit sapien interdum eu. Vestibulum at quam                               luctus, varius velit quis, suscipit purus. Nam interdum urna id purus ullamcorper porttitor. Suspendisse eros dui,                               dignissim ac libero ac, semper imperdiet tellus. Praesent dictum iaculis mi eget porta. Vivamus vulputate libero ac                              tincidunt tempor. Aenean placerat consectetur ullamcorper.                                                                                                                     
+                      </p>    
+                  </div>
+              </article>
+          </section>
+      </section>
+      
+      <aside class="topsidebar">
+          <article>
+              <h2>Top Sidebar</h2>
+              <p>tempor. Aenean placerat consectetur ullamcorper.</p>
+          </article>
+      </aside>
+      <aside class="middlesidebar">
+          <article>
+              <h2>Middle Sidebar</h2>
+              <p>tempor. Aenean placerat consectetur ullamcorper.</p>
+          </article>
+      </aside>
+      <aside class="bottomsidebar">
+          <article>
+              <h2>Bottom Sidebar</h2>
+              <p>tempor. Aenean placerat consectetur ullamcorper.</p>
+          </article>
+      </aside>
+      
+      
+      <footer class="mainfooter">
+          <p>Copyright &copy; <a href="#" title="Peter Clarke Designs">PeterClarkeDesigns.com</a></p>
+      </footer>
+      
+
+  </div>
+      
+
+      
+      
+     
+    <script src="http://code.jquery.com/jquery.js"></script>
+
+  </body>
+</html>
