47677ef66db33d4b830fdad25c2f775906225d56
designstudio_2 <designstudio_2@192.168.1.195>
2013-08-22 15:59:20 +0100

    Added index file

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brighton Comic Arts Festival</title>
-    <link rel="stylesheet" href="core.css" type="text/css" />
+    <link rel="stylesheet" href="assets/css/core.css" type="text/css" />
+    <script src="assets/js/modernizr.min.js"></script>
   </head>
   
   
@@ -15,66 +16,60 @@
           
           <img src="assets/img/logo-by-itself.gif" alt="Brighton Comic Arts Festival Logo" />
           
-          <nav>
+          <nav class="topnav">
               <ul>
                   <li><a href="#">Home</a></li>
                   <li><a href="#">About</a></li>
                   <li><a href="#">Events</a></li>
                   <li><a href="#">Merchandise</a></li>
-                  <li><a href="#">Contact Us</a></li>
+                  <li id="lastnav"><a href="#">Contact Us</a></li>
               </ul>
           </nav>
       </header>
       
       <section class="maincontent">
-          <section class="content">
-              <article class="toparticle">
-                  <header class="articleheader">
-                      <h2><a href="#" title="Article post">First Post</a></h2>
+              <section class="topsection">
+                  <header class="sectionheader">
+                      <h2><a href="#" title="Section post">First Post</a></h2>
                   </header>
-                  <footer class="articlefoot">
-                      <p>This article written by Peter</p>
+                  <footer class="sectionfoot">
+                      <p>This section written by Peter</p>
                   </footer>
-                  <div class="articlecontent">
+                  <div class="sectioncontent">
                       <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor mi, porta non viverra eu, lacinia a dolor.                                   Curabitur vel molestie augue. Suspendisse ultricies velit odio, a hendrerit sapien interdum eu. Vestibulum at quam                               luctus, varius velit quis, suscipit purus. Nam interdum urna id purus ullamcorper porttitor. Suspendisse eros dui,                               dignissim ac libero ac, semper imperdiet tellus. Praesent dictum iaculis mi eget porta. Vivamus vulputate libero ac                              tincidunt tempor. Aenean placerat consectetur ullamcorper.                                                                                                                     
                       </p>    
                   </div>
-              </article>
-              <article class="bottomarticle">
-                  <header class="articleheader">
-                      <h2><a href="#" title="Article post">Second Post</a></h2>
+              </section>
+              <section class="bottomsection">
+                  <header class="sectionheader">
+                      <h2><a href="#" title="Section post">Second Post</a></h2>
                   </header>
-                  <footer class="articlefoot">
-                      <p>This article written by Peter</p>
+                  <footer class="sectionfoot">
+                      <p>This section written by Peter</p>
                   </footer>
-                  <div class="articlecontent">
+                  <div class="sectioncontent">
                       <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor mi, porta non viverra eu, lacinia a dolor.                                   Curabitur vel molestie augue. Suspendisse ultricies velit odio, a hendrerit sapien interdum eu. Vestibulum at quam                               luctus, varius velit quis, suscipit purus. Nam interdum urna id purus ullamcorper porttitor. Suspendisse eros dui,                               dignissim ac libero ac, semper imperdiet tellus. Praesent dictum iaculis mi eget porta. Vivamus vulputate libero ac                              tincidunt tempor. Aenean placerat consectetur ullamcorper.                                                                                                                     
                       </p>    
                   </div>
-              </article>
-          </section>
+              </section>
       </section>
-      
-      <aside class="topsidebar">
-          <article>
-              <h2>Top Sidebar</h2>
-              <p>tempor. Aenean placerat consectetur ullamcorper.</p>
-          </article>
-      </aside>
-      <aside class="middlesidebar">
-          <article>
-              <h2>Middle Sidebar</h2>
-              <p>tempor. Aenean placerat consectetur ullamcorper.</p>
-          </article>
-      </aside>
-      <aside class="bottomsidebar">
-          <article>
-              <h2>Bottom Sidebar</h2>
-              <p>tempor. Aenean placerat consectetur ullamcorper.</p>
-          </article>
-      </aside>
+      <section class="sidebar">
+          <h1>Featured Artists</h1>
+          <aside class="topsidebar">
+              <section>
+                  <h2>Top Sidebar</h2>
+                  <p>tempor. Aenean placerat consectetur ullamcorper.</p>
+              </section>
+          </aside>
+          <aside class="bottomsidebar">
+              <section>
+                  <h2>Bottom Sidebar</h2>
+                  <p>tempor. Aenean placerat consectetur ullamcorper.</p>
+              </section>
+          </aside>
+      </section>
       
       
       <footer class="mainfooter">
